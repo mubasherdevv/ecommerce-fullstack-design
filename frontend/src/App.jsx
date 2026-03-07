5ca4a83daf5be0b791ce9d2637a110776d6c3534
Mubasher-Dev <whatsappbot11@hotmail.com>
2026-03-08 00:53:56 +0500

    feat: add frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,27 +5,54 @@ import HomePage from './pages/HomePage';
 import ProductListingPage from './pages/ProductListingPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import AdminDashboardPage from './pages/AdminDashboardPage';
+import ProductEditPage from './pages/ProductEditPage';
+import PrivateRoute from './components/PrivateRoute';
+import AdminRoute from './components/AdminRoute';
+
 import { CartProvider } from './context/CartContext';
+import { ProductProvider } from './context/ProductContext';
+import { AuthProvider } from './context/AuthContext';
 import './index.css';
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/products" element={<ProductListingPage />} />
-              <Route path="/products/:id" element={<ProductDetailPage />} />
-              <Route path="/cart" element={<CartPage />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </CartProvider>
+    <AuthProvider>
+      <ProductProvider>
+        <CartProvider>
+          <Router>
+            <div className="flex flex-col min-h-screen">
+              <Navbar />
+              <main className="flex-1">
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/products" element={<ProductListingPage />} />
+                  <Route path="/products/:id" element={<ProductDetailPage />} />
+                  <Route path="/cart" element={<CartPage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/register" element={<RegisterPage />} />
+                  
+                  {/* Private Routes */}
+                  <Route path="" element={<PrivateRoute />}>
+                    <Route path="/profile" element={<ProfilePage />} />
+                  </Route>
+
+                  {/* Admin Routes */}
+                  <Route path="" element={<AdminRoute />}>
+                    <Route path="/admin" element={<AdminDashboardPage />} />
+                    <Route path="/admin/product/:id/edit" element={<ProductEditPage />} />
+                  </Route>
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+          </Router>
+        </CartProvider>
+      </ProductProvider>
+    </AuthProvider>
   );
 }
 
