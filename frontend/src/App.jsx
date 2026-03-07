cf84078d768c03cf11a80fb050b428d76dad018b
Mubasher-Dev <whatsappbot11@hotmail.com>
2026-03-07 23:33:15 +0500

    feat: add frontend/src/App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProductListingPage from './pages/ProductListingPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import CartPage from './pages/CartPage';
+import { CartProvider } from './context/CartContext';
+import './index.css';
+
+function App() {
+  return (
+    <CartProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-1">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/products" element={<ProductListingPage />} />
+              <Route path="/products/:id" element={<ProductDetailPage />} />
+              <Route path="/cart" element={<CartPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
+  );
+}
+
+export default App;
